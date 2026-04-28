bc54e42bcff9123f780b83c4323489ec9e6fcbcf
KevinDouglas <douglas.kevin201@gmail.com>
2026-04-28 13:38:56 -0300

    aula sobre variaveis

---
+++ n1_nc4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 25;
+    int quantidade = 1;
+    float altura = 1.86;
+    double peso = 70.0;
+    char letra = 'k';
+    char nome[20] = "Kevin";
+
+
+    idade = 25;
+
+    printf(main\n);
+    return 0;
+}

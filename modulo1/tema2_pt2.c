b711742a9fe203c0898781aa8264301fb302d029
KevinDouglas <douglas.kevin201@gmail.com>
2026-05-21 02:54:07 -0300

    criando ficha de aluno

--- modulo1/tema2_pt2.c
+++ modulo1/tema2_pt2.c
@@ -2,8 +2,9 @@
 
 int main(){
     int idade;
-    printf("Digite sua idade: \n");
+    printf("Digite sua idade: ");
     scanf("%d", &idade);
     printf("Sua idade eh: %d\n", idade);
+    
     return 0;
 }

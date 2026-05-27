c42e1650c15a53748a03177602fdc4add41392cc
KevinDouglas <douglas.kevin201@gmail.com>
2026-05-27 03:12:53 -0300

    exercicio de if / else em C.

--- modulo1/modulo2/if_else.c
+++ modulo1/modulo2/if_else.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 
+int main() {
+    // Declarando as variáveis dentro do main
+    unsigned int estoque;
+    double capital;
+    double valor_total;
 
+    printf("Analise de Estoque\n");
+    printf("Digite a quantidade em estoque: \n");
+    scanf(" %u", &estoque); // Espaço antes do % para limpar buffer
 
-unsigned int estoque;
-double capital;
+    printf("Digite o valor do produto: \n");
+    
+    scanf(" %lf", &capital); 
 
-printf("Quantidade no estoque: \n");
-scanf("%u", &estoque);
+    valor_total = estoque * capital;
 
+    printf("\n=== Relatorio do Produto ===\n");
+
+    
+    if (estoque < 10) {
+        printf("Status do estoque: ALERTA! Estoque Baixo. \n");
+    } else {
+        printf("Status do estoque: OK (Adequado).\n");
+    }
+
+    
+    if (valor_total > 5000.00) {
+        printf("Alerta Financeiro: Capital de giro acumulado alto (R$ %.2f)!\n", valor_total);
+    }
+
+    return 0;
+}
